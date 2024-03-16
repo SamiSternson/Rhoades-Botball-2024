10db2518aac7f77549603e23ffea3625ccc986c5
SamiSternson <131320116+SamiSternson@users.noreply.github.com>
2024-03-16 11:56:30 -0700

    3/16/24 Weekend Session Upload

--- No Comment/Isaac/Line/src/main.c
+++ No Comment/Isaac/Line/src/main.c
@@ -6,24 +6,40 @@ void servo(int port, float targetangle, float time);
 int main()
 {
     //Main Servo Limit 1800
-    //3/10 successes
+    //SETUP: 45 degree rotation left from robot facing toward large space opening, 8 in. from back table corner (underneath)
+    //1550 O 750 C box servo
     create3_connect();
     create3_wait();
-	servo(3, 0, 1000);
-    servo(3, 1400, 2000);
     create3_rotate_degrees(-45, 90);
     create3_wait();
     create3_drive_straight(0.645, 0.46);
     create3_wait();
-    create3_rotate_degrees(-50, 45);
+    create3_rotate_degrees(-45, 45);
     create3_wait();
     create3_drive_straight(-0.09, 0.46);
     create3_wait();
-	servo(2, 400, 3);
+    servo(2, 300, 3);
     msleep(1000);
-    create3_drive_straight(0.05, 0.05);
+    create3_drive_straight(0.035, 0.05);
     create3_wait();
     weeblewobble();
+    create3_rotate_degrees(40, 45);
+    servo(2, 600, 1);
+    create3_drive_straight(0.15, 0.15);
+    create3_wait();
+    create3_rotate_degrees(-15, 45);
+    create3_wait();
+    while (create3_sensor_bump(4) != 1) {
+    	create3_velocity_set_components(0.1, 0);
+    }
+    create3_execute_next_command_immediately();
+    create3_rotate_degrees(20, 45);
+    create3_wait();
+    while (create3_sensor_bump(2) != 1) {
+    	create3_velocity_set_components(0.1, 0);
+    }
+    create3_execute_next_command_immediately();
+    create3_wait();
     /*
 	servo(2, 800, 1);
     create3_rotate_degrees(30, 45);
@@ -39,56 +55,36 @@ int main()
     create3_wait();
     //Drive up to edge of pipe area (Distance Not Measured)
     */
-    
+
     /*
-    ---SUDO CODE (NO REAL CODE)---
+    ---SUDO CODE (NOT REAL CODE)---
     -Use servos to dump pipes in tubes; Requires bottom mechanism
     */
     return 0;
 }
 
 void weeblewobble(){
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);    
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,0.46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,46);
-    create3_rotate_degrees(2,151);
-    create3_rotate_degrees(-2,151);
-    create3_drive_straight(-0.000636,0.46);
-    create3_drive_straight(0.000635,46);
-    
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151); 
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+    create3_rotate_degrees(1,151);
+    create3_rotate_degrees(-1,151);
+
     create3_wait();
 }
 
