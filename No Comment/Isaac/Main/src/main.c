0f0c6fff776080139471f47c1b15cc5128395dc4
SamiSternson <131320116+SamiSternson@users.noreply.github.com>
2024-03-25 15:10:52 -0700

    3/23/24 Weekend Session Upload

--- No Comment/Isaac/Main/src/main.c
+++ No Comment/Isaac/Main/src/main.c
@@ -6,16 +6,45 @@ int main()
 {
     create3_connect();
     create3_wait();
-    create3_drive_straight(0.09, 0.46);
+    create3_drive_straight(0.054 ,0.46);
     create3_wait();
-    create3_rotate_degrees(45, 141);
+    servo(0, 200, 2);
+    msleep(1000);
+    servo(0, 1800, 2);
+    create3_drive_straight(0.475, 0.46);
     create3_wait();
-    /*
-    servo(3, 0, 1);
-    servo(3, 1400, 2);
-    create3_rotate_degrees(-45, 141);
+    create3_rotate_degrees(-90, 162);
     create3_wait();
-    */
+    create3_drive_straight(0.07, 0.46);
+    create3_wait();
+    create3_rotate_degrees(90, 162);
+	create3_wait();
+    create3_rotate_degrees(20, 162);
+    create3_wait();
+    create3_drive_straight(0.30, 0.46);
+    create3_wait();
+    create3_rotate_degrees(-80, 162);
+    create3_wait();
+    while (create3_sensor_bump(4) == 0) {create3_velocity_set_components(0.1, 0);}
+    create3_execute_next_command_immediately();
+    create3_rotate_degrees(60, 162);
+    create3_wait();
+    //
+    int repeat = 1;
+    while (repeat == 1) {
+    	int irsense = create3_sensor_ir(6);
+        if (irsense > 1000) {repeat = 0;}
+        create3_velocity_set_components(0.05, 0);
+        msleep(10);
+    }
+    create3_execute_next_command_immediately();
+    create3_drive_straight(-0.10, 0.46);
+    create3_wait();
+    create3_rotate_degrees(-7, 162);
+    create3_wait();
+    servo(0, 200, 2);
+    servo(0, 1800, 2);
+    create3_wait();
     return 0;
 }
 
